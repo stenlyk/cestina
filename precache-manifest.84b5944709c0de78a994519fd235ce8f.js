6553ea26b3eeba17a8d32f11a3dfe037e7cdbf66
Stanislav Kurinec <stenly.kurinec@gmail.com>
2020-05-02 13:38:06 +0200

    Updates

---
+++ precache-manifest.84b5944709c0de78a994519fd235ce8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a4ef4a552b341ef4001020008e0ecb0",
+    "url": "/cestina/index.html"
+  },
+  {
+    "revision": "5939f6678ab724f746db",
+    "url": "/cestina/static/css/2.f933cb1f.chunk.css"
+  },
+  {
+    "revision": "ed50abc890408921c74d",
+    "url": "/cestina/static/css/main.e51bfbef.chunk.css"
+  },
+  {
+    "revision": "5939f6678ab724f746db",
+    "url": "/cestina/static/js/2.af732397.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cestina/static/js/2.af732397.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed50abc890408921c74d",
+    "url": "/cestina/static/js/main.a22f01eb.chunk.js"
+  },
+  {
+    "revision": "2f4c329273b7d2fb948d",
+    "url": "/cestina/static/js/runtime-main.8a251aa2.js"
+  }
+]);
